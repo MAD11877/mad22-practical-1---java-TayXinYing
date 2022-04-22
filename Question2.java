d0e6b6a804e336da44338aff0a0c22aace547adf
TayXinYing <104066372+TayXinYing@users.noreply.github.com>
2022-04-23 02:41:06 +0800

    Update Question2.java

--- Question2.java
+++ Question2.java
@@ -2,6 +2,8 @@ import java.util.Scanner;
 
 public class Question2
 {
+  public static void main(String[] args)
+  {
     Scanner in = new Scanner(System.in);
     
     System.out.print("Input weight in kg: ");
